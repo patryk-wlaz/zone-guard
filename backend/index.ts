4555a5b98fe9bf55191288a4e41390fdca3ae4b7
Patryk Wlaź <patryk.wlaz@gmail.com>
2024-05-14 13:29:38 +0200

    basic setup

---
+++ backend/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import dotenv from 'dotenv'
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
